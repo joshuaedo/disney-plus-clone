37c1ea7b886f7f2524fc95d3dcacf6ae892ef216
Joshua <joshua.edo01@gmail.com>
2023-02-17 11:43:54 +0100

    install react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <>
+        {/* <Navbar /> */}
+        <Routes>
+          <Route path="/" element={""} />
+        </Routes>
+      </>
+    </div>
+  );
 }
 
 export default App;
